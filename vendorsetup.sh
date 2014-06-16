efb04ea4e16e2821ced29acdc39f3561ed3d50a9
xiaognol <317065820@qq.com>
2014-06-16 09:10:01 +0800

    add support for vitural-keys vibration and c8815 patchs

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,9 @@
 # This whole script is a hack. Hoping to replace it soon.
+if [[ "$PATCHES_HUAWEI_C8815" -eq 1 ]]
+then
+  sh device/huawei/c8815/patches/apply.sh
+  echo ""
+  echo "***************** HUAWEI C8815 *****************"
+  echo ""
+fi
 add_lunch_combo mk_c8815-userdebug
